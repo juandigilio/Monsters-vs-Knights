2f4f82be883028af2780037eff411df916c8cdc3
Juan Digilio <juanignaciodigilio@gmail.com>
2024-10-14 23:45:47 -0300

    Added LevenController

---
+++ Monsters vs Knights/Assets/scripts/LevelController.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public static event Action OnAnimationWon;
+    public static event Action OnAnimationLost;
+
+
+
+    void Update()
+    {
+        
+    }
+}
