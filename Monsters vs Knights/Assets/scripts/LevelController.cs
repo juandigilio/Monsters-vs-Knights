5d28006836894216c959bcffc064379cc3d89986
Juan Digilio <juanignaciodigilio@gmail.com>
2024-10-17 14:22:22 -0300

    Update LevelController.cs

--- Monsters vs Knights/Assets/scripts/LevelController.cs
+++ Monsters vs Knights/Assets/scripts/LevelController.cs
@@ -6,13 +6,14 @@ using UnityEngine.TextCore.Text;
 public class LevelController : MonoBehaviour
 {
     [SerializeField] private List<Enemy> enemies = new List<Enemy>();
+    [SerializeField] private Player player;
 
     public static event Action OnPlayerWon;
 
 
     void Update()
     {
-        CheckWiningCondition();
+        CheckEnemiesStatus();
     }
 
     public List<Enemy> GetEnemies()
@@ -20,16 +21,36 @@ public class LevelController : MonoBehaviour
         return enemies;
     }
 
-    private void CheckWiningCondition()
+    public Vector3 GetPlayerPosition()
     {
+        return player.GetPosition();
+    }
+
+    private void CheckEnemiesStatus()
+    {
+        bool allEnemiesDead = true;
+
         foreach (Enemy enemy in enemies)
         {
             if (!enemy.IsDead())
             {
-                return;
+                allEnemiesDead = false;
+
+                enemy.SetPlayerDistance(Vector3.Distance(enemy.GetPosition(), player.GetPosition()));
+            }
+            else
+            {
+                enemies.Remove(enemy);
             }
         }
 
-        OnPlayerWon?.Invoke();
+        if (allEnemiesDead)
+        {
+            OnPlayerWon?.Invoke(); 
+        }
+        else
+        {
+            return;
+        }
     }
 }
